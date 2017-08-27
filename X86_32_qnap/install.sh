b5a3026fd379cff60fe85624a6abef58a05b2f9a
Wolfgang Willinghöfer <wolfgang@vm-debian-8.elv.lan>
2017-08-27 15:57:01 +0200

    delete old native qnap vesion

--- X86_32_qnap/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-mkdir -p /opt/hm
-cp -R packages/lighttpd/* /opt/hm
-cp -R packages-eQ-3/HS485D/* /opt/hm
-cp -R packages-eQ-3/RFD/* /opt/hm
-cp -R packages-eQ-3/WebUI/* /opt/hm
-cp -R packages-eQ-3/LinuxBasis/* /opt/hm
